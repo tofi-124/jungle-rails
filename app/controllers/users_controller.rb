c55bad4707892fd4166a83e3bb9cf22f5c701ce7
tofi-124 <tofikahm@yahoo.com>
2022-02-09 05:31:31 +0000

    user spec

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,8 @@ class UsersController < ApplicationController
   end
   
   def create
-    user = User.new(user_params)
-    if user.save
-      session[:user_id] = user.id
+    if @user = User.authenticate_with_credentials(params[:email], params[:password])
+      session[:user_id] = @user.id
       redirect_to '/'
     else
       redirect_to '/signup'
@@ -15,7 +14,7 @@ class UsersController < ApplicationController
   private
   
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :last_name, :email, :password, :password_confirmation)
   end
   end 
   
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,26 @@
 class User < ActiveRecord::Base
   has_secure_password
+  validates_uniqueness_of :email, :case_sensitive => false  
+  
+  def self.authenticate_with_credentials(email, password)
+    email.strip!
+    email.downcase!
+    user = User.find_by_email(email)
+    if user && user.authenticate(password)
+      user
+    else
+      nil
+    end
+  end 
+  
+  validates :password, presence: true, length: {minimum: 5}
+  validates :password_confirmation, presence: true, length: {minimum: 5}
+  validates :email, presence: true,  uniqueness: { case_sensitive: false }
+  validates :name, presence: true
+  validates :last_name, presence: true
+  before_save :downcase_fields
+
+  def downcase_fields
+    self.email.downcase!
+  end
 end
