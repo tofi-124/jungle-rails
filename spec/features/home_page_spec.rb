51f47992605da4f796febcebdc1d482b372dc57c
tofi-124 <tofikahm@yahoo.com>
2022-02-10 01:46:22 +0000

    Feature Spec: Home Page

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -1,6 +1,5 @@
 require 'rails_helper'
-
-RSpec.feature "Product Details", type: :feature, js:true  do
+RSpec.feature "User navigates to index page", type: :feature, js: true do
 
   before :each do
     @category = Category.create! name: 'Apparel'
@@ -9,17 +8,15 @@ RSpec.feature "Product Details", type: :feature, js:true  do
       @category.products.create!(
         name:  Faker::Hipster.sentence(3),
         description: Faker::Hipster.paragraph(4),
-        image: open_asset('apparel1.jpg'),
         quantity: 10,
         price: 64.99
       )
     end
   end
 
-  scenario "All products are shown" do
+  scenario " All products are shown" do
     visit root_path
-    page.find('.products').first(:link, "Details").click
-    expect(page).to have_css '.product-detail', count: 1
     save_screenshot
+    expect(page).to have_css 'article.product'
   end
 end
